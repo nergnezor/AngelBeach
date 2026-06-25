78433a4164754eb91919f23fd00fae4079efef6a
nergnezor <erik.rosengren@uxstream.com>
2026-06-25 20:09:08 +0200

    fix: upgrade build settings to V7 for UE5.7 compatibility

--- Source/BeachVolleyballEditor.Target.cs
+++ Source/BeachVolleyballEditor.Target.cs
@@ -6,7 +6,7 @@ public class BeachVolleyballEditorTarget : TargetRules
 	public BeachVolleyballEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("BeachVolleyball");
 	}
